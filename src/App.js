d83ddec007b829f177a23e03c72af439f7fef48e
CKNKP <kesarinandankumar2001@gmail.com>
2023-12-29 15:34:12 +0530

    Logo Design

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./Components/Login";
+import Login2 from "./Components/Login2";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Login></Login> */}
+      <Login2></Login2>
+    </div>
+  );
+}
+
+export default App;
